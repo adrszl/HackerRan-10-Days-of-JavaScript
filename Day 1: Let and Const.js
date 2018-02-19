1976bfc8a5fa8ccc6524e0b696c76b70b58e1b48
Adrian Szlegel <adrian95999@gmail.com>
2018-02-19 12:46:56 +0100

    Done! :fire:

---
+++ Day 1: Let and Const.js
@@ -0,0 +1,8 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    var r = readLine();
+    // Print the area of the circle:
+    const PI = Math.PI;
+    console.log(PI * r * r);
+    // Print the perimeter of the circle:
+    console.log(2 * PI * r);
