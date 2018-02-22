20988b5da1985dc88536bb36689144067000a982
Adrian Szlegel <adrian95999@gmail.com>
2018-02-22 14:36:40 +0100

    Done! :fire:

---
+++ Day 3: Arrays.js
@@ -0,0 +1,18 @@
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    for (var i = 0, largest = 0, secondLargest = 0; i < nums.length; ++i) {
+        if (nums[i] > largest) {
+            secondLargest = largest;
+            largest = nums[i];
+        }
+        else if (nums[i] > secondLargest && nums[i] < largest) {
+            secondLargest = nums[i];
+        }
+    }
+    return secondLargest;
+}
